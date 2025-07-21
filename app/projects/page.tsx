fd4b96eb9fadb3f655378672d94a417f82c19859
SELVAPRAVEEN-05 <selvapraveen2005@gmail.com>
2025-07-21 21:29:44 +0530

    re check

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
+import Blogix from '@/components/assets/blogix.png';
+import FoodWeb from '@/components/assets/FoodWeb.png';
+import MovieWeb from '@/components/assets/movieWeb.png';
+import RP from '@/components/assets/RP.png';
 import ProjectCard from '@/components/projectCard';
 import { useTheme } from 'next-themes';
 import Link from 'next/link';
-import FoodWeb from '@/components/assets/FoodWeb.png';
-import MovieWeb from '@/components/assets/movieWeb.png';
-import Blogix from '@/components/assets/blogix.png';
-import RP from '@/components/assets/RP.png';
 
 export default function Projects() {
   const { theme } = useTheme();
@@ -14,29 +14,20 @@ export default function Projects() {
   const projectsData = [
     {
       title: "Movie Web - Movie Search & Watchlist web app",
-      description:"Movie search and movie app using TMDB API where users can search, filter, sort, watch trailers, and manage a watchlist.",
-      tags: [
-        "React",
-        "Next.js",
-        "Hero UI",
-        "Tailwind",
-        "TMDB API",
-        "Vercel",
-        "TypeScript",
-        "Framer Motion",
-      ],
+      description: "Movie search and movie app using TMDB API where users can search, filter, sort, watch trailers, and manage a watchlist.",
+      tags: ["React", "Next.js", "Hero UI", "Tailwind", "TMDB API", "Vercel", "TypeScript", "Framer Motion",],
       githubLink: "https://github.com/SELVAPRAVEEN-05/Movie-Watchlist-Web",
       liveLink: "https://movie-watchlist-web.vercel.app/",
-      imageUrl:MovieWeb
+      imageUrl: MovieWeb
     },
     {
       title: "Blogix - AI-Powered Blogging Platform",
       description:
         "InsightFul is a fullstack tool that helps saas developers collect valuable user feedback and insights to improve your product and delight your customers.",
-      tags: ["React", "Next.js", "Tailwind", " PostgreSQL","Vector Db", "Typescript", "Vercel", "Prisma"],
+      tags: ["React", "Next.js", "Tailwind", " PostgreSQL", "Vector Db", "Typescript", "Vercel", "Prisma"],
       githubLink: "https://github.com/MayankUghade/InsightFul",
       liveLink: "https://insightful-two.vercel.app/",
-      imageUrl:Blogix
+      imageUrl: Blogix
     },
     {
       title: "Food Web - Food Delivery Web App",
@@ -45,28 +36,26 @@ export default function Projects() {
       tags: ["React", "Next.js", "Hero UI", "Tailwind", "MUI", "Vercel", "TypeScript"],
       githubLink: "https://github.com/SELVAPRAVEEN-05/food-web",
       liveLink: "https://food-web-flax.vercel.app/",
-      imageUrl:FoodWeb
+      imageUrl: FoodWeb
     },
-    
+
     {
       title: "Event Manager - Event Booking & Points System",
-      description:"Developed an event management app to replace Google Forms, boosting student participation by 30%",
-      tags: ["React", "vite", "MUI","Node", "MySQL", "Express", "Axios", "Tailwind","Figma"],
+      description: "Developed an event management app to replace Google Forms, boosting student participation by 30%",
+      tags: ["React", "vite", "MUI", "Node", "MySQL", "Express", "Axios", "Tailwind", "Figma"],
       githubLink: "https://github.com/MayankUghade/Epic-Energize",
       liveLink: "https://epic-energize.vercel.app/",
-      imageUrl:RP
+      imageUrl: RP
     },
   ];
 
   return (
     <div className="min-h-screen py-20 w-full flex flex-col justify-center">
-      
+
       <div className="bg-primary h-[1px] w-3/5 my-7"></div>
-      {/* Heading */}
       <h2 className="text-2xl sm:text-4xl font-serif text-center">
-       A small selection of{" "} <span className="text-purple-600 font-bold">Recent projects</span>
+        A small selection of{" "} <span className="text-purple-600 font-bold">Recent projects</span>
       </h2>
-      {/* Bottom Divider */}
       <div className="w-full flex justify-end">
         <div className="bg-primary h-[1px] w-3/5 my-7"></div>
       </div>
